031fd23f13ccfb69090f583a8557bc12d814b9e6
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-02-22 18:31:28 +0200

    class Seat converted to record

--- Cinema Room REST Service/task/src/cinema/model/Seat.java
+++ Cinema Room REST Service/task/src/cinema/model/Seat.java
@@ -1,19 +1,4 @@
 package cinema.model;
 
-public class Seat {
-    final int row;
-    final int column;
-
-    public Seat(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
+public record Seat(int row, int column) {
 }
