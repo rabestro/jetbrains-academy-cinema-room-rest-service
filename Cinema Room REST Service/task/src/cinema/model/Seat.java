ddc81d63cef540050f99cdb7760569ef4824ce18
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-02-22 19:52:36 +0200

    Add code to pass second test

--- Cinema Room REST Service/task/src/cinema/model/Seat.java
+++ Cinema Room REST Service/task/src/cinema/model/Seat.java
@@ -4,4 +4,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 public record Seat(@Min(1) @Max(9) int row, @Min(1) @Max(9) int column) {
+    public int getPrice() {
+        return row < 5 ? 10 : 8;
+    }
 }
